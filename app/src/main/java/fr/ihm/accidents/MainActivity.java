2c72fbc10e8f18ea5de020fc76fc0c498139f7cc
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-03-31 15:15:27 +0200

    Base project

---
+++ app/src/main/java/fr/ihm/accidents/MainActivity.java
@@ -0,0 +1,16 @@
+package fr.ihm.accidents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
