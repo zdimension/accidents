62a1dcb3d73b89ca7d13ff2b5e6253f40a5d7268
MarcelMARSAISLACOSTE <marcel.marsais-lacoste@etu.univ-amu.fr>
2021-03-31 16:38:09 +0200

    Activity "VictimeTemoin" + Intents

--- app/src/main/java/fr/ihm/accidents/VictimeTemoin.java
+++ app/src/main/java/fr/ihm/accidents/VictimeTemoin.java
@@ -9,34 +9,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+public class VictimeTemoin extends AppCompatActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_victime_temoin);
 
-        final TextView geolocalisationTextView = (TextView) findViewById(R.id.text_view_id);
+        final TextView geolocalisationTextView = (TextView) findViewById(R.id.textView);
 
-        final Button button = findViewById(R.id.button_id);
-        button.setBackgroundColor(Color.GREEN);
+        final Button button = findViewById(R.id.button);
+        button.setBackgroundColor(Color.GRAY);
         button.setTextColor(Color.BLACK);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intentVictimeTemoin = new Intent(MainActivity.this, VictimeTemoin.class);
-                startActivity(intentVictimeTemoin);
+
             }
         });
 
-        final Button button2 = findViewById(R.id.button_id2);
-        button2.setBackgroundColor(Color.RED);
+        final Button button2 = findViewById(R.id.button2);
+        button2.setBackgroundColor(Color.MAGENTA);
         button2.setTextColor(Color.BLACK);
         button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intentVictimeTemoin = new Intent(MainActivity.this, VictimeTemoin.class);
-                startActivity(intentVictimeTemoin);
+
             }
         });
     }
